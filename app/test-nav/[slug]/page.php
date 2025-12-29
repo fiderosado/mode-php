ede06b0f95534fc9cd62f48df6819b01aa6a2675
Fidel Remedios Rosado <fiderosado@gmail.com>
2025-12-28 20:06:37 -0500

    Fix routes of files

--- app/test-nav/[slug]/page.php
+++ app/test-nav/[slug]/page.php
@@ -9,7 +9,7 @@ $searchParams = searchParams();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SerJS Navigation</title>
     <script src="../../SerJS/SerJS.js"></script>
-    <script src="../../SerJS/navigation/navigation.js"></script>
+    <script src="../../SerJS/core/SerJSNavigation.js"></script>
 </head>
 <body>
     <h1>SerJS Navigation slug</h1>
